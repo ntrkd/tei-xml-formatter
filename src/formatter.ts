fb8b6cb0c89c6be1ed6831709f69f13a8fc23599
networkydev <140664254+networkydev@users.noreply.github.com>
2025-10-04 19:16:53 -0400

    Expose debug variables (remove in prod)

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,7 +1,24 @@
 import * as vscode from 'vscode';
 
 export class Formatter implements vscode.DocumentFormattingEditProvider {
+    /**
+     * helper func to debug, delete in prod
+     * @param charr character number on line 0 to get
+     * @returns a string for the return of getText(), undefined returns ""
+     */
+    getChar(line: number, charr: number): string {
+        let charia = vscode.window.activeTextEditor?.document.getText(new vscode.Range(new vscode.Position(line, charr), new vscode.Position(line, charr+1))) ?? "";
+        return charia;
+    } 
+
     provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+        // Entire part is to expose stuff to the debug REPL. Delete in prod
+        let getChar = this.getChar;
+        (globalThis as any).vscode = vscode;
+        (globalThis as any).Range = vscode.Range;
+        (globalThis as any).Position = vscode.Position;
+        (globalThis as any).document = document;
+
         let firstLine = document.lineAt(0);
         let tedit: vscode.TextEdit = vscode.TextEdit.delete(new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, firstLine.range.end.character)));
         
@@ -13,5 +30,4 @@ export class Formatter implements vscode.DocumentFormattingEditProvider {
 
         return arr;
     }
-
 }
