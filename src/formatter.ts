c2d9e09e7b099cccb965208053eb6a3a2b26fe2a
networkydev <140664254+networkydev@users.noreply.github.com>
2025-10-08 11:15:41 -0400

    Add logic to convert tokens from saxes to text

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,7 +1,9 @@
 import * as vscode from 'vscode';
 import { Lexer } from './parser/lexer';
 import { Token } from './parser/token';
-import { json } from 'stream/consumers';
+import { json } from 'saxes';
+import { format } from 'path';
+import { partialDeepStrictEqual } from 'assert';
 
 export class Formatter implements vscode.DocumentFormattingEditProvider {
     /**
@@ -22,11 +24,88 @@ export class Formatter implements vscode.DocumentFormattingEditProvider {
         (globalThis as any).Position = vscode.Position;
         (globalThis as any).document = document;
 
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            vscode.window.showErrorMessage("No active editor found.");
-            return; // or throw an error
+        let formatted: string[] = [];
+        let indent: number = 0;
+
+        const parser = new SaxesParser();
+        const he = require("he");
+
+        parser.on("error", function (e) {
+            console.error(e);
+            vscode.window.showErrorMessage("Error: " + e.message);
+            return;
+        });
+        
+        parser.on("xmldecl", dec => { // Always the first line in the XML document
+            formatted.push(`<?xml version="${dec.version}"${dec.encoding !== undefined ? ` encoding="${dec.encoding}"` : ``}${dec.standalone !== undefined ? ` standalone="${dec.standalone}"` : ``}?>`);
+        });
+        
+        parser.on("doctype", doc => {
+            formatted.push(`<!DOCTYPE${doc}>`);
+        });
+        
+        parser.on("processinginstruction", pi => {
+            formatted.push(`${this.tabLines(indent)}<?${pi.target}${pi.body !== "" ? ` ${pi.body}` : ``}?>`);
+        });
+        
+        parser.on("cdata", cdata => {
+            formatted.push(`${this.tabLines(indent)}<![CDATA[${cdata}]]>`);
+        });
+        
+        parser.on("opentag", tag => {
+            let formattedTag = `<${tag.name}`; 
+
+            const size = Object.keys(tag.attributes).length;
+
+            if (size === 0) { // Is a regular closing tag
+                if (tag.isSelfClosing) { formattedTag += `/`; indent--; }
+                formattedTag += `>`;
+                formatted.push(`${this.tabLines(indent)}${formattedTag}`);
+                indent++;
+                return;
+            }
+            for (const key in tag.attributes) {
+                formattedTag += ` ${key}="${tag.attributes[`${key}`]}"`;
+            }
+
+            if (tag.isSelfClosing) { formattedTag += `/`; }
+            formattedTag += `>`;
+            formatted.push(`${this.tabLines(indent)}${formattedTag}`);
+
+            indent++;
+            console.log(indent);
+        });
+        
+        parser.on("closetag", tag => {
+            if (tag.isSelfClosing) { return; } // This is already taken care of within the opentag handler
+            indent--;
+            
+            formatted.push(`${this.tabLines(indent)}</${tag.name}>`);
+        });
+        
+        parser.on("comment", comment => {
+            formatted.push(`${this.tabLines(indent)}<!--${comment}-->`);
+        });
+        
+        parser.on("text", text => {
+            let parsedText = text.replace(/[\n\t]/g, "");
+            parsedText = he.encode(parsedText);
+            if (parsedText === "") { return; }
+            
+            formatted.push(`${this.tabLines(indent)}${parsedText}`);
+        });
+
+        parser.write(document.getText(new vscode.Range(new vscode.Position(0, 0), new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).range.end.character)))).close();
+
+        let formatText = formatted.join("\n");
+
+        const folder = vscode.workspace.workspaceFolders?.[0];
+        if (!folder) {
+            vscode.window.showErrorMessage("No workspace open");
+            return;
         }
+        const fileUri = vscode.Uri.joinPath(folder.uri, "formatted.xml");
+        vscode.workspace.fs.writeFile(fileUri, Buffer.from(formatText, "utf8"));
 
         return;
     }
