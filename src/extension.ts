a66614923ac066afa0383d54186942c19547dc38
networkydev <140664254+networkydev@users.noreply.github.com>
2025-12-21 16:14:11 -0500

    yo code

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,26 @@
-import * as vscode from 'vscode'; // Import VS Code API
-import { Formatter } from './formatter';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('tei-xml-formatter is now active!');
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "tei-xml-fmt" is now active!');
 
-	let formatter = new Formatter;
-	let selector: vscode.DocumentSelector = { scheme: 'file', language: 'xml' };
-	const formatRegister = vscode.languages.registerDocumentFormattingEditProvider(selector, formatter);
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('tei-xml-fmt.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from tei-xml-fmt!');
+	});
 
-	context.subscriptions.push(formatRegister);
+	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
